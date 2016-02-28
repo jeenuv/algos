8630d27f56c42c21a89b2f337b4a3de2092c79a2
Jeenu Viswambharan <jeenuv@gmail.com>
2016-02-28 13:00:54 +0000

    Fix function name in insertion sort

--- selection_sort.py
+++ selection_sort.py
@@ -2,14 +2,15 @@
 
 import sort_utils as su
 
-def insertion_sort(array):
+def selection_sort(array):
     # Compare element at i with everything to its right. When a number less than
     # i is found, we swap. The slice [0:i] always remain sorted. Stop sorting
     # when there were no swaps made
+    n = len(array)
     i = 0
-    while i < N:
+    while i < n:
         j = i + 1
-        while j < N:
+        while j < n:
             if array[j] < array[i]:
                 su.xchg(array, i, j)
             j += 1
@@ -22,7 +23,7 @@ if __name__ == "__main__":
     array = su.get_array(N)
     su.print_index(N)
     su.print_array("start", array)
-    array = insertion_sort(array)
+    array = selection_sort(array)
     su.check_array(array)
 
 # vim: set tw=80 sw=4:
